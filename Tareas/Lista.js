f6cd783d8be1885459bd6bfc3818f04bdbe35e22
Alejandro Zamudio <62968090+alejandro061001@users.noreply.github.com>
2022-10-30 17:51:35 +0000

    Add files via upload

---
+++ Tareas/Lista.js
@@ -0,0 +1,14 @@
+const lista=[
+     "Alejandro Zamudio",
+     21,
+    true,
+    new Date(2001, 09, 05),
+     {
+        titulo: "El pez de papel que aprendio a nadar",
+        autor: "Jonathon A. Flaun",
+        Fecha: new Date (2007, 0, 06),
+        url: "https://www.amazon.com/-/es/Jonathon-Flaum/dp/8483580268"
+    }
+]
+
+console.log(lista)
