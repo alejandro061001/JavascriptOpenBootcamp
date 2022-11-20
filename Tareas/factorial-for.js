0dbb7242509cefad2b1d0f0aa96b676afad151de
Alejandro Zamudio <62968090+alejandro061001@users.noreply.github.com>
2022-11-20 02:05:40 +0000

    Add files via upload

---
+++ Tareas/factorial-for.js
@@ -0,0 +1,11 @@
+// vamos a factorizar un numero con for
+
+let num= 10
+   
+    for (var i = num - 1; i >= 1; i--) {
+      num *= i;
+    }
+    console.log( num);
+  
+  
+
---
+++ Tareas/factorial-while.js
@@ -0,0 +1,12 @@
+// Fctorizar con el bucle while
+
+let num = 10
+let fac = 1
+while (num != 0) {
+    fac = fac * num;
+    num = num - 1;
+}
+console.log(fac);
+
+
+
