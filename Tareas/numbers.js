58ba51e2fad96407e3677c8e3d374989fd6de721
Alejandro Zamudio <62968090+alejandro061001@users.noreply.github.com>
2022-11-20 17:43:07 +0000

    Add files via upload

---
+++ Tareas/numbers.js
@@ -0,0 +1,11 @@
+let alturaCent = 183
+let alturaMetr= 1.83
+let pesoKilo= 90.5
+let altRedonArri = Math.round(alturaMetr);
+let pesoRedonAbajo= Math.floor(pesoKilo);
+//aqui se obtiene el numero maximo en Javascript
+let maxvalue = Number.MAX_VALUE + 1;
+let compare = Number.MAX_VALUE;
+
+console.log (maxvalue=== compare);
+
